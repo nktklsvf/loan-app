dfa5fba48683945e41deede131cd3ee1f95df60f
Nikita Kolosov <nktklsvf@gmail.com>
2022-07-15 12:05:34 +0300

    Removing, resolving loans

--- LoanApplication/Controllers/LoanController.cs
+++ LoanApplication/Controllers/LoanController.cs
@@ -27,6 +27,49 @@ namespace LoanApplication.Controllers
             LoanView loanView = new LoanView(loan, actions);
             return View(loanView);
         }
+        public ActionResult Remove(int id)
+        {
+            Console.WriteLine(id);
+            Loan loan = _db.Loans.Find(id);
+            if (loan != null)
+            {
+                _db.Loans.Remove(loan);
+                _db.SaveChanges();
+            }
+            return Redirect("/Loan");
+        }
+        public ActionResult AddAction(int id)
+        {
+            AddLoanAction addLoanAction = new AddLoanAction();
+            addLoanAction.LoanId = id;
+            return View(addLoanAction);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddAction(AddLoanAction obj)
+        {
+            if (ModelState.IsValid)
+            {
+                LoanAction loanAction = new LoanAction();
+                User giverUser = _db.Users.Single(user => user.Name == obj.GiverUserName);
+                User takerUser = _db.Users.Single(user => user.Name == obj.TakerUserName);
+                Console.WriteLine(obj.LoanId);
+                Loan loan = _db.Loans.Single(l => l.Id == obj.LoanId);
+                if (giverUser != null && takerUser != null && loan != null)
+                {
+                    loanAction.GiverUser = giverUser;
+                    loanAction.TakerUser = takerUser;
+                    loanAction.Purpose = obj.Purpose;
+                    loanAction.Amount = obj.Amount;
+                    loanAction.Loan = loan;
+                    _db.LoanActions.Add(loanAction);
+                    _db.SaveChanges();
+                    return Redirect("/Loan/Show/" + obj.LoanId);
+                }
+            }
+            return View(obj);
+        }
+
         public IActionResult Create()
         {
             return View();
